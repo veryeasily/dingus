6ee53d1fd4ea9251e3a7e11a221db50085f16c8e
Nicholas Packauskas <Nick.Packauskas@gmail.com>
2018-12-08 13:29:04 -0600

    Update server.js

--- server.js
+++ server.js
@@ -39,52 +39,53 @@ app.use(async ctx => {
       axios.post(GROUPME_API_PATH, {
         bot_id: BOT_ID,
         text: "Nick is superior to Tim"
-      } else if (0.75 < rand && rand <0.9){
-        axios.post(GROUPME_API_PATH, {
-          bot_id: BOT_ID,
-          text: "Nick is vastly better than Tim"
-        })
-      } else if (0.45 <= rand && rand <=0.75){
-        axios.post(GROUPME_API_PATH, {
-          bot_id: BOT_ID,
-          text: "Nick is  better than Tim"
-        })
-      } else if (0.40 <= rand && rand <0.45){
-        axios.post(GROUPME_API_PATH, {
-          bot_id: BOT_ID,
-          text: "Stalemate"
-        })
-      } else if (0.2 <= rand && rand <0.4){
-        axios.post(GROUPME_API_PATH, {
-          bot_id: BOT_ID,
-          text: "Tim is better than Nick"
-        })
-      } else if (0.1 <= rand && rand <0.2){
-        axios.post(GROUPME_API_PATH, {
-          bot_id: BOT_ID,
-          text: "Tim is vastly better than Nick"
-        })
-      } else if (0.01 <= rand && rand <0.1){
-        axios.post(GROUPME_API_PATH, {
-          bot_id: BOT_ID,
-          text: "Tim is superior to Nick"
-        })
-      } else if (rand <0.01){
-        axios.post(GROUPME_API_PATH, {
-          bot_id: BOT_ID,
-          text: "Tim is better than Tim"
-        })
-      } else {
-        const msg = text.match(/@dingus\s(.*)/i)[1]
-        const resp = await axios.get(
-          `http://txtingus:5000/${encodeURIComponent(msg)}`
-        )
-        const temp2 = await axios.post(GROUPME_API_PATH, {
-          bot_id: BOT_ID,
-          text: resp.data
-        })
-    ctx.body = 'Hello World'
-  }
-})
+      })
+    } else if (0.75 < rand && rand <0.9){
+      axios.post(GROUPME_API_PATH, {
+        bot_id: BOT_ID,
+        text: "Nick is vastly better than Tim"
+      })
+    } else if (0.45 <= rand && rand <=0.75){
+      axios.post(GROUPME_API_PATH, {
+        bot_id: BOT_ID,
+        text: "Nick is  better than Tim"
+      })
+    } else if (0.40 <= rand && rand <0.45){
+      axios.post(GROUPME_API_PATH, {
+        bot_id: BOT_ID,
+        text: "Stalemate"
+      })
+    } else if (0.2 <= rand && rand <0.4){
+      axios.post(GROUPME_API_PATH, {
+        bot_id: BOT_ID,
+        text: "Tim is better than Nick"
+      })
+    } else if (0.1 <= rand && rand <0.2){
+      axios.post(GROUPME_API_PATH, {
+        bot_id: BOT_ID,
+        text: "Tim is vastly better than Nick"
+      })
+    } else if (0.01 <= rand && rand <0.1){
+      axios.post(GROUPME_API_PATH, {
+        bot_id: BOT_ID,
+        text: "Tim is superior to Nick"
+      })
+    } else if (rand <0.01){
+      axios.post(GROUPME_API_PATH, {
+        bot_id: BOT_ID,
+        text: "Tim is better than Tim"
+      })
+    } else {
+      const msg = text.match(/@dingus\s(.*)/i)[1]
+      const resp = await axios.get(
+        `http://txtingus:5000/${encodeURIComponent(msg)}`
+      )
+      const temp2 = await axios.post(GROUPME_API_PATH, {
+        bot_id: BOT_ID,
+        text: resp.data
+      })
+      ctx.body = 'Hello World'
+    }
+  })
 
 app.listen(3000)
